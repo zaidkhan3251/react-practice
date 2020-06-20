cab845d51619543c438cda5ce5e1367d38826cb0
zaidkhan3251 <48489044+zaidkhan3251@users.noreply.github.com>
2020-06-19 22:13:30 -0700

    ag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import child from './child';
 
 function App() {
   return (
--- src/child.js
+++ src/child.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function child() {
   return (
-    <div>
+    <div className="container">
         <h1>Expense Tracker</h1>
         <h3>Your Balance<br/> $260</h3>
-        <div>
+        <div className="expense-container">
             <h3>INCOME<br/>$500</h3>
             <h3>EXPENSE<br/>$240</h3>
         </div>
