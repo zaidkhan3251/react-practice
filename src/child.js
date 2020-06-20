e50907d537b97b75921c9da12a86226da45e73f2
zaidkhan3251 <48489044+zaidkhan3251@users.noreply.github.com>
2020-06-20 00:24:23 -0700

    add

--- src/child.js
+++ src/child.js
@@ -4,34 +4,49 @@ import './App.css';
 
 
 function Child() {
-  return (
-    <div className="container">
-        <h1 className="text-container">Expense Tracker</h1>
-        <h3>Your Balance<br/> $260</h3>
-        <div className="expense-container">
-            <h3>INCOME <br/> $500</h3>
-            <h3>EXPENSE<br/> $240</h3>
-        </div>
-        <h3>History</h3>
-        <hr/>
-        <h3>Add new transaction</h3>
-        <hr/>
+    return (
+        <div className="container">
+            <h1 className="text-container">Expense Tracker</h1>
+            <h3>Your Balance<br /> $260</h3>
+            <div className="expense-container">
+                <h3>INCOME <br /> $500</h3>
+                <h3>EXPENSE<br /> $240</h3>
+            </div>
+            <h3>History</h3>
+            <hr />
+            <ul className="transaction-list">
 
-        <form className="transaction-form">
-            <label>
-                Enter Description<br/>
-                <input type="text" />
-            </label>
-            <br/>
-            <label>
-                Enter Amount<br/>
-                <input type="number" />
-            </label>
-            <br/>
-            <input type="submit" value="add Transaction"/>
-        </form>
-    </div>
-  );
+                <li>
+                    <span>cash</span>
+                    <span>+500</span>
+                </li>
+                <li>
+                    <span>cash</span>
+                    <span>+500</span>
+                </li>
+                <li>
+                    <span>cash</span>
+                    <span>+500</span>
+                </li>
+            </ul>
+            <h3>Add new transaction</h3>
+            <hr />
+
+            <form className="transaction-form">
+                <label>
+                    Enter Description<br />
+                    <input type="text" required />
+                </label>
+                <br />
+                <label>
+                    Enter Amount<br />
+                    <input type="number" />
+                </label>
+                <br />
+                <input type="submit" value="add Transaction" />
+            </form>
+        </div>
+    );
 }
 
 export default Child;
