f7b2e76f6e00e1329c33ca9b4dddda6d215c29ab
zaidkhan3251 <48489044+zaidkhan3251@users.noreply.github.com>
2020-06-19 21:49:19 -0700

    add

---
+++ src/child.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function child() {
+  return (
+    <div>
+        <h1>Expense Tracker</h1>
+        <h3>Your Balance<br/> $260</h3>
+        <div>
+            <h3>INCOME<br/>$500</h3>
+            <h3>EXPENSE<br/>$240</h3>
+        </div>
+      
+    </div>
+  );
+}
+
+export default child;
